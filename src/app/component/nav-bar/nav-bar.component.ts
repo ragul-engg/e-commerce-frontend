ec967c583d299b11cbd5bd89550a0764e9276912
PRABHUDEESHWARAN <prabhudeeshwaranganesh@gmail.com>
2024-03-16 22:28:32 +0530

    add DashBoard to navBar

--- src/app/component/nav-bar/nav-bar.component.ts
+++ src/app/component/nav-bar/nav-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/service/auth.service';
 import { SearchResultComponent } from '../search-result/search-result.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
@@ -8,10 +9,16 @@ import { SearchResultComponent } from '../search-result/search-result.component'
   styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent {
-  constructor(public authService: AuthService,private searchResult:SearchResultComponent) {}
+  constructor(private router:Router ,public authService: AuthService,private searchResult:SearchResultComponent) {}
 
   toggle(){
     this.searchResult.isavailable=!this.searchResult.isavailable;
   }
+
+  handleDashboard()
+  {
+    this.router.navigate(['/admin'])
+  }
+  
 
 }
